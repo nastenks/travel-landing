7abf4f2b2f07eacd7d363cf616ebd46a5b05f7d9
nastenkos <52440055+nastenks@users.noreply.github.com>
2022-07-21 11:49:16 +0300

    LT-1
    header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BaseHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import BaseHeader from './components/BaseHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    BaseHeader,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    max-width: 1140px;
+    padding: 0 45px;
+    margin: 0 auto;
+
+   
+  }
 </style>
