c8dfffdc52f87b2ab1a541a7e6df6e17850f92a7
nastenkos <52440055+nastenks@users.noreply.github.com>
2022-07-21 13:37:58 +0300

    lt-2
    banner revision

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="banner">
     <div class="banner__text">
-
+      <h1 class="banner__title">Explore and <br> Travel</h1>
+      <p class="banner__descr">Holiday finder</p>
     </div>
     <div class="banner__image">
-      <img src="" alt="Picture on the banner">
+      <img src="/img/banner_image.png" alt="Picture on the banner">
     </div>
   </div>
 </template>
@@ -14,3 +15,52 @@ export default ({
   name: 'MainBanner',
 });
 </script>
+
+<style lang="scss">
+  .banner {
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 80px;
+
+    &__text {
+      color: #202336;
+      text-align: left;
+
+      h1 {
+        margin-bottom: 74px;
+        font-family: 'Playfair Display', sans-serif;
+        font-weight: 700;
+        font-size: 64px;
+      }
+
+      &__descr {
+        font-family: 'Inter';
+        font-weight: 600;
+        font-size: 22px;
+      }
+
+      p {
+        &:before {
+          display: block;
+          position: absolute;
+          height: 2px;
+          width: 32px;
+          background: #202336;
+          bottom: -1px;
+          content: "";
+          left: 0;
+        }
+      }
+    }
+
+    &__image {
+      img {
+        position: absolute;
+        right: -100px;
+        width: 687px;
+        height: 654px;
+      }
+    }
+  }
+</style>
